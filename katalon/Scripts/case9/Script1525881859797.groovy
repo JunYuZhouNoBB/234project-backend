377c3d56b33bc5b118ad71080d1d8e333d1e6a40
JunYu Zhou <feijifeibudao@gmail.com>
2018-05-09 23:40:36 +0700

    test case9 not finished

---
+++ katalon/Scripts/case9/Script1525881859797.groovy
@@ -0,0 +1,46 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://34.215.218.253:9998/')
+
+WebUI.setText(findTestObject('case9_Page_ProjectBackend/input_username'), 'user')
+
+WebUI.setText(findTestObject('case9_Page_ProjectBackend/input_password'), 'user')
+
+WebUI.click(findTestObject('case9_Page_ProjectBackend/button_Login'))
+
+WebUI.click(findTestObject('case9_Page_ProjectBackend/button_add to cart'))
+
+WebUI.click(findTestObject('case9_Page_ProjectBackend/button_add to cart'))
+
+WebUI.click(findTestObject('case9_Page_ProjectBackend/a_Carts            2'))
+
+WebUI.click(findTestObject('case9_Page_ProjectBackend/p_Total price  20150 THB'))
+
+WebUI.setText(findTestObject('case9_Page_ProjectBackend/input_amount'), '2')
+
+WebUI.click(findTestObject('case9_Page_ProjectBackend/p_Total price  40150 THB'))
+
+WebUI.closeBrowser()
+
---
+++ katalon/Test Suites/test suite9.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>After selecting the products, if the user moves to the Carts page, all the selected products is shown in the cart page. The user can change the amount of products for each type of product, and the total price will be changed regarding to the input value.</description>
+   <name>test suite9</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T23:35:21</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>68751a0a-032d-46d7-9f83-e1147e06817d</testSuiteGuid>
+   <testCaseLink>
+      <guid>d1eb792b-bc7c-424b-b53e-5f1ffbc19ad0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/case9</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>7c1e11ac-d1ce-4fd4-8ac3-84790b3eeb4a</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>2-3</value>
+         </iterationEntity>
+         <testDataId>Data Files/user</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>7c1e11ac-d1ce-4fd4-8ac3-84790b3eeb4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>08cb2703-47b3-4a5e-8db0-bc1cc1acb6e5</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>7c1e11ac-d1ce-4fd4-8ac3-84790b3eeb4a</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>5e426f9a-f172-4e6c-8af0-51601147b9b5</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
