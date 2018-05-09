cad5a18fb6da42205998c95bc302b410c7f79907
JunYu Zhou <feijifeibudao@gmail.com>
2018-05-09 22:44:29 +0700

    test case 4 redo

--- katalon/Test Suites/test suite4.ts
+++ katalon/Test Suites/test suite4.ts
@@ -1,22 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-<<<<<<< HEAD
-   <description>If the user name and password is not correct, the error message, “Username/password is incorrect” is shown</description>
-=======
    <description></description>
->>>>>>> master
    <name>test suite4</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-05-09T18:41:29</lastRun>
+   <lastRun>2018-05-09T22:43:53</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <testSuiteGuid>9e6ab15a-1edd-4ffc-bd54-fa3cb61b6fbd</testSuiteGuid>
+   <testSuiteGuid>9ba3bfe2-66cb-44ca-97d6-9bb3cc535845</testSuiteGuid>
    <testCaseLink>
-      <guid>c617c113-fc5d-48a1-9ecc-9df256e44c7f</guid>
+      <guid>558033bd-2586-47b2-9d7c-9dfd1bf5a03a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/case4</testCaseId>
