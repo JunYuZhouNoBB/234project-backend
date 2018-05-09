33d264569192cf09d58337b7e2b080b7857a3a5b
JunYu Zhou <feijifeibudao@gmail.com>
2018-05-09 23:04:00 +0700

    test case 8
    When the user enters the Products page, and clicked at the add to cart button, the button will be changed to already added, and the number of selected products is shown next to the Carts menu.

---
+++ katalon/Test Suites/test suite8.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>When the user enters the Products page, and clicked at the add to cart button, the button will be changed to already added, and the number of selected products is shown next to the Carts menu.</description>
+   <name>test suite8</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T23:02:22</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>323b3522-475d-4814-951c-6a1162915ca6</testSuiteGuid>
+   <testCaseLink>
+      <guid>06341065-b884-46ea-8986-4193e4095b93</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/case8</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>62ad438e-6fbe-4d7c-8200-67358656b599</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>2-3</value>
+         </iterationEntity>
+         <testDataId>Data Files/user</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>62ad438e-6fbe-4d7c-8200-67358656b599</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>90a07006-700d-4a81-8709-b6f7983edcdc</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>62ad438e-6fbe-4d7c-8200-67358656b599</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>2dfabcac-14a9-42bf-a5ee-eeddec7560fb</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
