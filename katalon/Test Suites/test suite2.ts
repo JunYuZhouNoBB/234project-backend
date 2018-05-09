57864960d95585a067c08b968594caee98128434
JunYu Zhou <feijifeibudao@gmail.com>
2018-05-09 18:30:56 +0700

    test case 2

---
+++ katalon/Test Suites/test suite2.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test suite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T18:29:29</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>19888763-421a-4c4c-874b-7b5e36c36321</testSuiteGuid>
+   <testCaseLink>
+      <guid>596ecee1-88b6-453a-8901-443435a3f944</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/case2</testCaseId>
+      <testDataLink>
+         <combinationType>MANY</combinationType>
+         <id>00c65cfe-0a81-4edb-9173-8c25349198ad</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>2-3</value>
+         </iterationEntity>
+         <testDataId>Data Files/user</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>00c65cfe-0a81-4edb-9173-8c25349198ad</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>95754178-2692-4d48-a7de-9678a51441ee</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>00c65cfe-0a81-4edb-9173-8c25349198ad</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>327992e4-be84-4776-952f-a882cc3ec554</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
