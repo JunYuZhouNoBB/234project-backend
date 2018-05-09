2204a722bdd583473929078b2ad1a9eed7ce6c46
JunYu Zhou <feijifeibudao@gmail.com>
2018-05-09 23:50:12 +0700

    test case 10
    If the user clicks confirm button, the transaction will be added.

---
+++ katalon/Test Suites/test suite10.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test suite10</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-05-09T23:49:04</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>275ff870-d372-4006-8cfd-256e5987c5a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>8a9136fb-369e-483f-9bce-d0e40f9e5f27</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/case10</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>b30c17fb-0256-424d-bbff-f0a2b63fe938</id>
+         <iterationEntity>
+            <iterationType>RANGE</iterationType>
+            <value>2-3</value>
+         </iterationEntity>
+         <testDataId>Data Files/user</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>b30c17fb-0256-424d-bbff-f0a2b63fe938</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>221b757b-cba9-4ebe-958d-1bda3924ab3b</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>b30c17fb-0256-424d-bbff-f0a2b63fe938</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>91f76cd6-0b3b-485b-befb-ceaab1c1ba6a</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
